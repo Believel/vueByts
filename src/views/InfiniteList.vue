9d9b164d3f072789311b6d0bbf7f128a86ebefce
tgd <zjrzpp0414@163.com>
2019-11-01 16:05:20 +0800

    long list

---
+++ src/views/InfiniteList.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    虚拟列表：https://mp.weixin.qq.com/s?__biz=MzI1ODk2Mjk0Nw==&mid=2247484725&idx=1&sn=c9356addc239225f21233aa6fdbc01e4&chksm=ea016659dd76ef4f4fc0e029d0b6a69f4f18552f8146629698768d160bdac256cd3be6fb6d7f&mpshare=1&scene=1&srcid=&sharer_sharetime=1572590812832&sharer_shareid=ae26eba62530acbb7d7a50a03413e4e1&key=88fff000e72a6bb76d9401d9b1ad0222340ac3b2a66549ac488233295d58235e85b19915f02354f2306fb9f85130a396ff8a5d86dc4d9da8e05c1991d3ceef147f7a5a5493d0578d30992e8870f1f247&ascene=1&uin=MTQxNzE3NDI4NQ%3D%3D&devicetype=Windows+10&version=62070152&lang=zh_CN&pass_ticket=1ESXCJKG63M73RMROQw9AkaIdmMHhOk2vWg7UX3NrypZA7ghyy%2F8rdIAB%2F10vAPT
+  </div>
+</template>
+
+<script lang="ts">
+  export default {
+  }
+</script>
+
+<style lang="scss" scoped>
+</style>
